ff5839c5ba545b75aff2d41387c8f9b94b82e926
Jonnas <jonnas.drumond@gmail.com>
2024-03-19 23:21:07 -0300

    6º atividade de PYTHON

---
+++ ex006.py
@@ -0,0 +1,6 @@
+from math import sqrt
+num = int(input('Digite um numero:'))
+dobro = num * 2
+triplo = num * 3
+raiz = sqrt(num)
+print(f'O dobro de {num} é -> {dobro}, o triplo é -> {triplo} e a raiz quarada é -> {raiz:.2f}')
