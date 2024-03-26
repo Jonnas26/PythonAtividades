a437103c6e704b49612f80082617c265315162ef
Jonnas <jonnas.drumond@gmail.com>
2024-03-25 23:15:48 -0300

    8º atividade de PYTHON

---
+++ ex008.py
@@ -0,0 +1,5 @@
+metro = int(input('Digite um valor em metros:'))
+cm = metro * 100
+mm = metro * 1000
+print(f'{metro} metros em Centimetros fica {cm} centimetros.')
+print(f'{metro} metros em Centimetros fica {mm} milimetros.')
