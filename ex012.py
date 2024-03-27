ab85e20533ab00fb054fc584b903afd7eca09097
Jonnas <jonnas.drumond@gmail.com>
2024-03-26 22:40:48 -0300

    12º atividade de PYTHON

---
+++ ex012.py
@@ -0,0 +1,3 @@
+preço = float(input('Qual o valor do produto:'))
+desconto = (preço * 5)/100
+print(f'O produto com preço de R${preço} terá um desconto de 5%\ne agora seu valor é de R${preço - desconto}')
