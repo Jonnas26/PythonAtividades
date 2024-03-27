3408b264f5bf5ea6bad1ebde9db014eaa7e1706c
Jonnas <jonnas.drumond@gmail.com>
2024-03-26 22:44:50 -0300

    13º atividade de PYTHON

---
+++ ex013.py
@@ -0,0 +1,3 @@
+salario = float(input('Qual seu salario atual:'))
+aumento = (salario * 15) / 100
+print(f'Seu novo salario deixara de ser R${salario} e \npassará ser R${salario + aumento} com 15% de aumento.')
