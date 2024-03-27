a50e3a5c1127c7b53beaf394e5b5f135e769ba10
Jonnas <jonnas.drumond@gmail.com>
2024-03-26 22:37:45 -0300

    11º ativiade de PYTHON

---
+++ ex011.py
@@ -0,0 +1,5 @@
+altura = int(input('Quanto de altura tem a parede :'))
+largura = int(input('Quanto de largura tem a parede :'))
+area = largura * altura
+tinta = (area) / 2
+print(f'Será necessario usar {tinta} litros de tinta para pintar essa parede de {area} metros')
