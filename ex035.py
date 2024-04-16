1ce8b7edb0b9c4f45dbd5d1ef2aac31e0980e0cf
Jonnas <jonnas.drumond@gmail.com>
2024-04-15 23:22:36 -0300

    35º atividade de PYTHON

---
+++ ex035.py
@@ -0,0 +1,8 @@
+a = int(input('Digite um numero:'))
+b = int(input('Digite um numero:'))
+c = int(input('Digite um numero:'))
+if a + b > c and b + c > a and c + a > b:
+    print('Da de formar um triangulo.')
+else:
+    print('Não da de formar um triangulo.')
+    
