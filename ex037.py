88bd3f8686b4b5b73bdd63d2ba036867dc92d81e
Jonnas <jonnas.drumond@gmail.com>
2024-04-16 23:19:02 -0300

    37º atividade de PYTHON

---
+++ ex037.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um numero:'))
+#Conversor de bases#
+print(f'O numero em outras bases fica :
+      \nBINARIO -> {bin(num)[2:]}\nOCTAL -> {oct(num)[2:]}\nHEXADECIMAL -> {hex(num)[2:]}')
