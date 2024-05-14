13f9928ec1358d939249748fa449ec67323f9574
Jonnas <jonnas.drumond@gmail.com>
2024-05-13 22:51:03 -0300

    49º atividade de PYTHON

--- ex048.py
+++ ex048.py
@@ -1,8 +1,9 @@
+#Fazendo a soma e contando todos os numeros impares entre 1 e 500#
 soma = 0
 cont = 0
 for c in range(1,501,2):
     if c % 3 == 0:
-        print(c,end=' ->')
+        print(c,end=' -> ')
         cont += 1
         soma = soma + c
 print('FIM')
---
+++ ex049.py
@@ -0,0 +1,3 @@
+numero = int(input('Digite um numero :'))
+for c in range(0,11):
+    print(f'{numero:>2} x {c:>2} = {numero * c}')
