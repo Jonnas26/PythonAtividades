3edc8c444fce37345a6c612ae443cf5e0b7fca51
Jonnas26 <jonnas.drumond@gmail.com>
2024-11-14 19:29:25 -0300

    66° atividade de PYTHON

--- ex066.py
+++ ex066.py
@@ -1,2 +1,8 @@
-print('teste')
-print('Teste2')
+soma =  cont = 0
+while True:
+    num = int(input('Digite um numero[999 para parar]:'))
+    if num == 999:
+        break
+    soma += num
+    cont += 1
+print(f'A soma dos  {cont} numeros digitados é de {soma}.')
