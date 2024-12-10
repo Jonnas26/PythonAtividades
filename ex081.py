14b71c19df2a4f99de2e171e264ece879ec19158
Jonnas26 <jonnas.drumond@gmail.com>
2024-12-10 01:20:44 -0300

    81° atividade de PYTHON

---
+++ ex081.py
@@ -0,0 +1,19 @@
+lista = []
+while True:
+    lista.append(int(input('Digite um numero: ')))
+    while True:
+        parar = str(input('Deseja parar? [S/N]')).strip().upper()[0]
+        if parar in 'SN':
+            break
+        print('DIGITE SIM OU NÂO')
+    if parar in 'S':
+        break
+print('==' * 20)
+print(f'Foi digitado um total de {len(lista)} elementos.')
+lista.sort(reverse=True)
+print(f'Em ordem decrescente fica {lista}')
+if 5 in lista:
+    print('tem 5')
+else:
+    print('Nao tem')
+    
