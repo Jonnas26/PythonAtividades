4583a8a253d50e43c6a2503d2d23cea885e526d5
Jonnas26 <jonnas.drumond@gmail.com>
2025-02-05 19:45:20 -0300

    102° atividade de PYTHON

---
+++ ex102.py
@@ -0,0 +1,21 @@
+def fatorial(n, show=False):
+    """CAlCULO DE FATORIAL
+
+    :param n: O valor a ser calculado o fatorial
+    :param show: (opcional) retorna ou não o calculo
+    :return: O valor do FATORIAL do numero N
+    """
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c,end=' ')
+            if c > 1:
+                print(' x ' ,end='')
+            else:
+                print(' = ' ,end='')
+        f *= c
+    return f
+
+
+print(fatorial(5))
+#help(fatorial)
