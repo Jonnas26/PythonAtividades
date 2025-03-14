02bb2978f6a8f89bf7c44a5f2192e89b13b585a1
Jonnas26 <jonnas.drumond@gmail.com>
2025-03-13 21:27:54 -0300

    107° atividade de PYTHON

---
+++ ex107.py
@@ -0,0 +1,7 @@
+#primeira atividade usando um modulos *util.py*
+import util
+
+preco = int(input('Digite um preço R$ :'))
+print(f'A metade do preço é {util.metade(preco):.2f}')
+print(f'O dobro do preço é {util.dobro(preco)}')
+print(f'Com 10% a mais fica {util.aumento10(preco)} (teve um aumento de R${util.aumento10(preco) - preco})')
---
+++ util.py
@@ -0,0 +1,10 @@
+def metade(num):
+    return num / 2
+
+
+def dobro(num):
+    return num * 2
+
+
+def aumento10(num):
+    return num + (num * 10)/100
